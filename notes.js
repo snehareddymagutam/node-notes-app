dd0133c6b2b81c35b84485b5b21b6f0bd8e87d1e
unknown <mnsnehareddy@gmail.com>
2019-09-25 19:15:50 -0500

    initial commit

---
+++ notes.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const getNotes = () =>{
+    return 'Your notes...'
+}
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    //if (duplicateNotes.length === 0) {
+        if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added!')
+    } else {
+        console.log('Note title taken!')
+    }
+}
+const removeNote = (title)=> {
+    const notes = loadNotes()
+    const notestokeep =notes.filter((note)=> note.title!= title)
+    if(notes.length >notestokeep.length){
+    saveNotes(notestokeep)
+    console.log(chalk.green('note removed'))
+    } else {
+        console.log(chalk.red('no note found'))
+
+    }
+}
+const listNotes = () =>{
+    const notes = loadNotes()
+    console.log(chalk.blue("Your Notes..."))
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
+const readNotes = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+    if(note)
+    {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+
+    }
+    else{
+        console.log(chalk.red("No note found ...."))
+    }
+}
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNotes: readNotes
+}
+
+
---
+++ utils.js
@@ -0,0 +1,9 @@
+console.log("utils.js")
+
+// var name = "sneha"
+// module.exports=name
+
+var add= function(x,y){
+    return x+y
+}
+module.exports=add
